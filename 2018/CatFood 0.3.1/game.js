24ce9ffe64171aec5de7ebdcf7565f0e8741f93a
YaroslavWeb <35300057+YaroslavWeb@users.noreply.github.com>
2018-12-05 17:14:32 +0800

    patch

--- 2018/CatFood 0.3.1/game.js
+++ 2018/CatFood 0.3.1/game.js
@@ -1,13 +1,39 @@
+
+    var inputState = {
+      RIGHT: false,
+      LEFT: false
+    }
+
+    var setKeyState = function (keyCode, isPressed) {
+      switch (keyCode) {
+        case 39:
+        case 68:
+          inputState.RIGHT = isPressed;
+          break;
+        case 37:
+        case 65:
+          inputState.LEFT = isPressed;
+          break;
+      }
+    };
+    var keydownHandler = (e) => {
+      setKeyState(e.which, true);
+    };
+    var keyupHandler = (e) => {
+      setKeyState(e.which, false);
+    };
+
+    document.addEventListener('keydown', keydownHandler);
+    document.addEventListener('keyup', keyupHandler);
+
 var game = {
 
-  dispay_game: document.createElement("canvas").getContext("2d"),
+  display_game: document.createElement("canvas").getContext("2d"),
   display_main: document.querySelector("canvas").getContext("2d"),
 
   sprites: {
-    background: undefined,
-    cat: undefined
+    background: undefined
   },
-
 
   start: function () {
     this.sprites.background = new Image();
@@ -19,14 +45,31 @@ var game = {
     this.run();
   },
 
-
   render: function () {
-    this.display_main.drawImage(this.dispay_game.canvas, 0, 0, this.dispay_game.canvas.width, this.dispay_game.canvas.height, 0, 0, this.display_main.canvas.width, this.display_main.canvas.height);
-    this.dispay_game.drawImage(this.sprites.background, 0, 0, game.dispay_game.canvas.width, game.dispay_game.canvas.height);
+    this.display_main.drawImage(this.display_game.canvas, 0, 0, this.display_game.canvas.width, this.display_game.canvas.height, 0, 0, this.display_main.canvas.width, this.display_main.canvas.height);
+    this.display_game.drawImage(this.sprites.background, 0, 0, game.display_game.canvas.width, game.display_game.canvas.height);
 
     //sprites
-    this.dispay_game.drawImage(this.sprites.cat, 10, 415, 95, 115);
+    this.display_game.drawImage(this.sprites.cat, cat.x, cat.y, cat.width, cat.height);
   },
+
+
+  renderButtons: function (buttons) {
+    var button, index;
+
+    this.display_game.fillStyle = "rgb(214,86,43,0)";
+    this.display_game.fillRect(0, 0, this.display_game.canvas.width, this.display_game.canvas.height);
+
+    for (index = buttons.length - 1; index > -1; --index) {
+
+      button = buttons[index];
+
+      this.display_game.fillStyle = button.color;
+      this.display_game.fillRect(button.x, button.y, button.width, button.height);
+    }
+  },
+
+
   resize: function () {
     game.display_main.canvas.width = Math.floor(document.documentElement.clientWidth - 32);
     if (game.display_main.canvas.width > document.documentElement.clientHeight) {
@@ -36,12 +79,62 @@ var game = {
   },
 
   run: function () {
+    this.physics();
     this.resize();
     this.render();
+    //if (device.mobile() || device.tablet()) {
+    this.renderButtons(controller.buttons);
 
     window.requestAnimationFrame(function () {
       game.run();
     });
+  },
+
+  physics: function () {
+
+
+    if (controller.buttons[0].active && game.cat.jumping == false) {
+      this.sprites.cat.velocity_y = -20;
+      this.sprites.cat.jumping = true;
+    }
+
+    if (controller.buttons[1].active || inputState.LEFT) {
+
+      this.sprites.cat.velocity_x -= 0.5;
+
+    }
+
+    if (controller.buttons[2].active || inputState.RIGHT) {
+
+      this.sprites.cat.velocity_x += 0.5;
+
+    }
+    // simulate gravity:
+    this.sprites.cat.velocity_y += 1.5;
+
+    // simulate friction:
+    this.sprites.cat.velocity_x *= 0.9;
+    this.sprites.cat.velocity_y *= 0.9;
+
+    this.sprites.cat.x += this.sprites.cat.velocity_x;
+    this.sprites.cat.y += this.sprites.cat.velocity_y;
+
+    if (this.sprites.cat.x + this.sprites.cat.width < 0) {
+
+      this.sprites.cat.x = this.sprites.display_game.canvas.width;
+
+    } else if (this.sprites.cat.x > game.display_game.canvas.width) {
+
+      this.sprites.cat.x = 0;
+
+    }
+
+    if (this.sprites.cat.y + this.sprites.cat.height > 150) {
+
+      this.sprites.cat.y = 150 - this.sprites.cat.height;
+      this.sprites.cat.jumping = false;
+
+    }
   }
 };
 
@@ -49,13 +142,24 @@ window.addEventListener("load", function () {
   game.start();
 });
 
-game.dispay_game.canvas.height = 600;
-game.dispay_game.canvas.width = 800;
+game.display_game.canvas.height = 600;
+game.display_game.canvas.width = 800;
 
 
+//Character and Items
+cat = {
+  x: 10,
+  y: 415,
+  width: 95,
+  height: 115,
+  sliding: true,
+  velocity_x: 0,
+  velocity_y: 0,
+}
 
 
-Button = function (x, y, width, height, color) {
+//MOBILE CONTROLLER
+var Button = function (x, y, width, height, color) {
 
   this.active = false;
   this.color = color;
@@ -67,63 +171,62 @@ Button = function (x, y, width, height, color) {
 }
 
 Button.prototype = {
+
   containsPoint: function (x, y) {
+
     if (x < this.x || x > this.x + this.width || y < this.y || y > this.y + this.width) {
+
       return false;
+
     }
     return true;
+
   }
 
 };
+var controller = {
 
-var Controller = {
+  buttons: [
+    new Button(0, 500, 800, 100, "rgb(109,80,255, 0.5)"), //slide
+    new Button(0, 0, 390, 500, "rgb(214,86,43, 0.5)"), //left
+    new Button(410, 0, 390, 500, "rgb(214,86,43, 0.5)") //right
+  ],
 
+  testButtons: function (target_touches) {
 
-    buttons: [
-
-      new Button(0, 160, 120, 120, "#f09000"),
-      new Button(380, 160, 120, 120, "#0090f0"),
-      new Button(520, 160, 120, 120, "#0090f0")
-
-    ],
-
-    testButtons: function (target_touches) {
-
-      var button, index0, index1, touch;
-
-      for (index0 = this.buttons.length - 1; index0 > -1; --index0) {
-
-        button = this.buttons[index0];
-        button.active = false;
-
-        for (index1 = target_touches.length - 1; index1 > -1; --index1) {
-
-          touch = target_touches[index1];
-
-          if (button.containsPoint((touch.clientX - display.bounding_rectangle.left) * display.buffer_output_ratio, (touch.clientY - display.bounding_rectangle.top) * display.buffer_output_ratio)) {
-
-            button.active = true;
-            break;
-
-          }
-
+    var button, index0, index1, touch;
+    for (index0 = this.buttons.length - 1; index0 > -1; --index0) {
+      button = this.buttons[index0];
+      button.active = false;
+      for (index1 = target_touches.length - 1; index1 > -1; --index1) {
+        touch = target_touches[index1];
+        if (button.containsPoint((touch.clientX - display.bounding_rectangle.left) * display.buffer_output_ratio, (touch.clientY - display.bounding_rectangle.top) * display.buffer_output_ratio)) {
+          button.active = true;
+          break;
         }
-
       }
-    },
-
-    touchEnd: function (event) {
-      event.preventDefault();
-      controller.testButtons(event.targetTouches);
-    },
-
-    touchMove: function (event) {
-      event.preventDefault();
-      controller.testButtons(event.targetTouches);
-    },
-
-    touchStart: function (event) {
-      event.preventDefault();
-      controller.testButtons(event.targetTouches);
     }
+  },
+
+  touchEnd: function (event) {
+
+    event.preventDefault();
+    controller.testButtons(event.targetTouches);
+
+  },
+
+  touchMove: function (event) {
+
+    event.preventDefault();
+    controller.testButtons(event.targetTouches);
+
+  },
+
+  touchStart: function (event) {
+
+    event.preventDefault();
+    controller.testButtons(event.targetTouches);
+
+  }
+
 };
