6db8fd879cf3c2da3222289d556e398a46db7d1c
YaroslavWeb <35300057+YaroslavWeb@users.noreply.github.com>
2019-01-01 17:22:28 +0800

    0.4.12

---
+++ 2018/CatFood 0.5/menu.js
@@ -0,0 +1,14 @@
+//Press start
+var meow = new Audio('assets/sounds/meow1.mp3');
+meow.volume = 0.2;
+
+var eating = new Audio('assets/sounds/eat.mp3');
+eating.volume = 0.1;
+
+$('.startGame').click(function() {
+  $('.container-fluid').removeClass('h-100');
+  $('#menu').hide();
+  meow.play();
+  $('#game').fadeIn(1000);
+});
+
