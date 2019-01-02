39ecfd2c9a304675cdd7b08b347ef4a89201e776
YaroslavWeb <35300057+YaroslavWeb@users.noreply.github.com>
2019-01-02 18:42:51 +0800

    Update game.js

--- 2019/CatFood 0.5/game.js
+++ 2019/CatFood 0.5/game.js
@@ -200,8 +200,8 @@ var game = {
         $('.score').html(game.score);
         //Play sound
         if (soundFlag) {
-          eating.volume = 0.2;
-          eating.play();
+          meow.volume = 0.2;
+          meow.play();
           soundFlag = false;
         }
       }
