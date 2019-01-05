a3f4bbfa778546f82a1e6d7a657f704dab9c04c4
YaroslavWeb <35300057+YaroslavWeb@users.noreply.github.com>
2019-01-05 22:16:44 +0800

    optim

---
+++ 2019/MasterFood 0.5/controller.js
@@ -0,0 +1,54 @@
+//MOBILE CONTROLLER
+var button1, button2;
+
+var el = document.getElementsByTagName('body')[0];
+
+el.addEventListener("touchstart", handler, false);
+el.addEventListener("touchend", handleEnd, false);
+el.addEventListener("touchmove", handler, false);
+
+function handler(e) {
+  button2 = !(button1 = (window.innerWidth / 2) < e.changedTouches[0].pageX);
+};
+
+function handleEnd(e) {
+  var touches = e.changedTouches;
+  if (window.innerWidth / 2 < touches[0].pageX)
+    button1 = false;
+  button2 = false;
+  if (window.innerWidth / 2 > touches[0].pageX) {
+    button2 = false;
+    button1 = false
+  }
+};
+//MOBILE CONTROLLER
+
+
+//KEYBOARD CONTROLLER
+var inputState = {
+  RIGHT: false,
+  LEFT: false
+}
+
+var setKeyState = function (keyCode, isPressed) {
+  switch (keyCode) {
+    case 39:
+    case 68:
+      inputState.RIGHT = isPressed;
+      break;
+    case 37:
+    case 65:
+      inputState.LEFT = isPressed;
+      break;
+  }
+};
+var keydownHandler = (e) => {
+  setKeyState(e.which, true);
+};
+var keyupHandler = (e) => {
+  setKeyState(e.which, false);
+};
+
+document.addEventListener('keydown', keydownHandler);
+document.addEventListener('keyup', keyupHandler);
+//KEYBOARD CONTROLLER
